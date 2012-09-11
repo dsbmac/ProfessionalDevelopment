c783ecf55f0324d3d5c7dd854ee3a8f7ea43f90b
Denis Mach <dsbmach@gmail.com>
2012-09-11 02:33:52 -0400

    logpuzzle

--- google-python-exercises/logpuzzle/logpuzzle.py
+++ google-python-exercises/logpuzzle/logpuzzle.py
@@ -18,15 +18,41 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
-
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
-  # +++your code here+++
-  
+  f = openFile(filename)
+  if f:
+    hostnames = extract_hostnames( f )
+    return sorted(hostnames, key=getSecondWord)
 
+def getSecondWord(gString):
+  match = re.search(r'-(\w+).jpg', gString)
+  if match:
+    return match.group(1)
+
+def openFile(filename):
+  try:
+    f = open(filename, 'rU')
+    return f
+  except IOError:
+    print 'Could not open file.'
+
+def extract_hostnames( f ):
+  urlList = []
+  lines = f.readlines()
+  for line in lines:
+    match = re.search(r'GET (\S+)', line )
+    if match:
+      gString = match.group(1)
+    if 'puzzle' in gString:
+      puzzleUrl = 'http://www.code.google.com'+gString
+      if puzzleUrl not in urlList:
+        urlList.append(puzzleUrl)
+  return urlList
+  
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads
   each image into the given directory.
@@ -35,8 +61,35 @@ def download_images(img_urls, dest_dir):
   with an img tag to show each local image file.
   Creates the directory if necessary.
   """
-  # +++your code here+++
-  
+  numberOfImages = len(img_urls)
+  imgNames = create_img_names(numberOfImages)
+  for i in xrange(numberOfImages):
+    urllib.urlretrieve(img_urls[i], imgNames[i])
+  create_index(imgNames)
+
+def create_index(imgNames):
+  html = create_html(imgNames)
+  try:
+    f = open('index.html', 'w')
+    try:
+      f.write(html)
+    finally:
+      f.close()
+  except IOErrror:
+    pass
+
+def create_html(imgNames):
+  result = '<html><body>\n'
+  for name in imgNames:
+    result += '<img src="'+ name[:-4] + '.jpg">'
+  result += '\n</body></html>'
+  return result
+
+def create_img_names(numberOfImages):
+  result = []
+  for i in xrange(numberOfImages):
+    result.append('img'+str(i)+'.jpg')
+  return result
 
 def main():
   args = sys.argv[1:]
@@ -57,5 +110,8 @@ def main():
   else:
     print '\n'.join(img_urls)
 
-if __name__ == '__main__':
-  main()
+##if __name__ == '__main__':
+##  main()
+
+urls = read_urls('place_code.google.com')
+download_images(urls,'')
