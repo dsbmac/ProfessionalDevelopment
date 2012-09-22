44c12df4cd213a03cf113cc0b7641a8edd204b35
Denis Mach <dsbmach@gmail.com>
2012-09-22 02:21:50 -0400

    cs254

--- MIT 6.001/1.3.rkt
+++ MIT 6.001/1.3.rkt
@@ -1,3 +1,11 @@
+;1.2
+( / (+ 5 
+       4
+       (- 2 (- 3 (+ 6 (/ 4 5)))))
+    (* 3 
+       (- 6 2) 
+       (- 2 7)))
+
 ;ex 1.3
 
 (define (sumSquareMax x y z)
@@ -5,4 +13,9 @@
         ((= (min x y z) y) (+ (expt x 2) (expt z 2)))
         ((= (min x y z) z) (+ (expt x 2) (expt y 2)))))
 
-(sumSquareMax 3 2 3)
+;1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(a-plus-abs-b 2 -3)
+
---
+++ Udacity CS253/helloworld/helloworld.py
@@ -0,0 +1,3 @@
+print 'Content-type: text/plain'
+print ' '
+print 'Hello, Udacity!'
