0a3cc85629bd1f2ca96f78b814451c2c2a67f9ab
Denis Mach <dsbmach@gmail.com>
2012-11-12 10:46:57 -0500

    mitx
    ps6

--- MITx 6.00x/ProblemSet6/ps6.py
+++ MITx 6.00x/ProblemSet6/ps6.py
@@ -45,8 +45,29 @@ def process(url):
 #======================
 
 # Problem 1
-
-# TODO: NewsStory
+class NewsStory:
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
+    
+    def getGuid(self):
+        return self.guid
+                
+    def getTitle(self):
+        return self.title
+        
+    def getSubject(self):
+        return self.subject
+        
+    def getSummary(self):
+        return self.summary
+        
+    def getLink(self):
+        return self.link
+        
 
 #======================
 # Part 2
