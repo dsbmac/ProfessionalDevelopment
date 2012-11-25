33dae7d942cade10319fe772333b7d9e88877fb4
Denis Mach <dsbmach@gmail.com>
2012-11-25 14:03:44 -0500

    cs212

--- Udacity CS212/poker.py
+++ Udacity CS212/poker.py
@@ -53,17 +53,85 @@ def hand_rank(hand):
     else:                                          # high card
         return (0, ranks)
 
-def test():
-    "Test cases for the functions in poker program"
+# -----------
+# User Instructions
+# 
+# Define two functions, straight(ranks) and flush(hand).
+# Keep in mind that ranks will be ordered from largest
+# to smallest.
+
+def straight(ranks):
+    "Return True if the ordered ranks form a 5-card straight."
+    return ranks[0] - ranks[-1] == 4 and len(set(ranks)) == 5
+
+def flush(hand):
+    "Return True if all the cards have the same suit."
+    return len(set(s  for r,s in hand)) == 1
+
+# -----------
+# User Instructions
+# 
+# Define a function, two_pair(ranks).
+
+def two_pair(ranks):
+    """If there are two pair, return the two ranks as a
+    tuple: (highest, lowest); otherwise return None."""
+    pairs = set(r  for r in ranks  if ranks.count(r) == 2)
+    if len(pairs) == 2:
+        return tuple(pairs)
+    else:
+        return None
+
+# -----------
+# User Instructions
+# 
+# Define a function, kind(n, ranks).
+
+def kind(n, ranks):
+    """Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand."""
+    for r in ranks:
+       if ranks.count(r) == n:
+            return r
+    return None             
+        
+def test2():
+    "Test cases for the functions in poker program."
     sf = "6C 7C 8C 9C TC".split() # Straight Flush
     fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
     fh = "TD TC TH 7C 7D".split() # Full House
-    assert poker([sf, fk, fh]) == sf
-    assert poker([fk, fh]) == fk
-    assert poker([fh, fh]) == fh
-    assert poker([sf]) == sf
-    assert poker([sf] + 99*[fh]) == sf
-    assert hand_rank(sf) == (8, 10)
-    assert hand_rank(fk) == (7, 9, 7)
-    assert hand_rank(fh) == (6, 10, 7)
+    fkranks = card_ranks(fk)
+    #tpranks = card_ranks(tp)
+    assert kind(4, fkranks) == 9
+    assert kind(3, fkranks) == None
+    assert kind(2, fkranks) == None
+    assert kind(1, fkranks) == 7
     return 'tests pass'
+    
+def card_ranks(hand):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
+    ranks.sort(reverse = True)
+    return ranks
+
+test2()
+
+##def test():
+##    "Test cases for the functions in poker program"
+##    sf = "6C 7C 8C 9C TC".split()
+##    fk = "9D 9H 9S 9C 7D".split()
+##    fh = "TD TC TH 7C 7D".split()
+##    assert straight([9, 8, 7, 6, 5]) == True
+##    assert straight([9, 8, 8, 6, 5]) == False
+##    assert flush(sf) == True
+##    assert flush(fk) == False
+##    return 'tests pass'
+##    assert poker([sf, fk, fh]) == sf
+##    assert poker([fk, fh]) == fk
+##    assert poker([fh, fh]) == fh
+##    assert poker([sf]) == sf
+##    assert poker([sf] + 99*[fh]) == sf
+##    assert hand_rank(sf) == (8, 10)
+##    assert hand_rank(fk) == (7, 9, 7)
+##    assert hand_rank(fh) == (6, 10, 7)
+##    return 'tests pass'
